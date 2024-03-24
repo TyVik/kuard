1f37b7df91e2df864a1b08f4d06d74c5b93f92a9
NagapetyanMargarita <mrnagapetyan@mail.ru>
2024-03-24 19:00:39 +0200

    kuard

---
+++ kuard/alerts/telegram.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import telebot
+import sys
+import configparser
+import subprocess
+
+load_dotenv('.env')
+tel_tok = os.getenv("TELEGRAM_TOKEN")
+if tel_tok is None:
+     os.environ["TELEGRAM_TOKEN"] = "7034741381:AAGfECxVwQSId3tXlJCm4dBWoUSjI1_sc7o"
+     tel_tok = "7034741381:AAGfECxVwQSId3tXlJCm4dBWoUSjI1_sc7o"
+bot = telebot.TeleBot(token=tel_tok)
+
+
+
+def alert_telegram(message):
+    token = os.environ.get("TELEGRAM_TOKEN")
+    chat_id = os.environ.get("TELEGRAM_ID")
+    if token and chat_id:
+        #bot.send_message(chat_id=chat_id, text=message)
+       message = message.replace(" ", "%20")
+       curl_command = f"curl 'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}'"
+       subprocess.run(curl_command, shell=True)
+
