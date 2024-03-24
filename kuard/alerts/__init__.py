7a57c8c39a960b02e609e1d15db60dace0590abd
NagapetyanMargarita <mrnagapetyan@mail.ru>
2024-03-24 19:54:54 +0200

    new

--- kuard/alerts/__init__.py
+++ kuard/alerts/__init__.py
@@ -1,17 +1,9 @@
 from .telegram import alert_telegram
-#, start_bot, load_env_variables
 import os
 import configparser
-"""
-def notify(message: str):
-    alert_telegram(message)
-"""
-#if os.path.exists("env_config.ini"):
- #  load_env_variables('env_config.ini')
+
 
 def notify(message: str):
-   # if not "telegram_chat_id" in os.environ:
-     #   start_bot()
     alert_telegram(message)
 
 
