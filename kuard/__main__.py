977a92922b5a6ef288919c0745bf379e0ce4057d
TyVik <tyvik8@gmail.com>
2024-04-08 21:30:51 +0300

    Fix ssh key usage.

--- kuard/__main__.py
+++ kuard/__main__.py
@@ -1,18 +1,11 @@
 import json
-import subprocess
-import base64
 import os
+import base64
 from kubernetes import client, config
 from kubernetes.client import V1Node, V1Pod
-import  paramiko
-import io
 import tempfile
-import re
 
-
-from paramiko import RSAKey
 from paramiko.client import SSHClient, AutoAddPolicy
-from dotenv import dotenv_values
 
 from kuard.alerts import notify
 from kuard.class_types import Pod, Container, Metrics
@@ -56,16 +49,9 @@ def get_ip(node: V1Node) -> str:
     return internal.address
 
 
-#def get_ssh_to_node(ip: str) -> SSHClient:
-def get_ssh_to_node(ip: str, private_key_str:str) -> SSHClient:
-    def get_private_key_for_ip(ip) -> str:
-        command = ['minikube', 'ssh-key']
-        completed = subprocess.run(command, capture_output=True)
-        return completed.stdout.decode().strip()
-
+def get_ssh_to_node(ip: str, private_key_str: str) -> SSHClient:
     ssh = SSHClient()
     ssh.set_missing_host_key_policy(AutoAddPolicy())  # Добавление ключа хоста (для примера, пропускает проверку ключа)
-    #private_key_file = get_private_key_for_ip(ip)
 
     with tempfile.NamedTemporaryFile(delete=False) as key_file:
         key_file.write(private_key_str.encode())
@@ -106,14 +92,11 @@ def check_rules(container: Container):
 if __name__ == "__main__":
     nodes = get_nodes()
     state = {get_ip(node): get_pods(node) for node in nodes}
-    value = dotenv_values('.env').get('IP_SSH')
-    decoded = base64.b64decode(value).decode()
-    ips = json.loads(decoded)
+    ssh_keys = json.loads(base64.b64decode(os.environ.get('IP_SSH')).decode())
     for node, pods in state.items():
-        ip=list(ips.keys())[0]
-        ssh_key = ips[ip]
-        #ssh = get_ssh_to_node(node)
-        ssh = get_ssh_to_node(ip,ssh_key)
+        ip = list(ssh_keys.keys())[0]
+        ssh_key = ssh_keys[ip]
+        ssh = get_ssh_to_node(ip, ssh_key)
         for pod in pods:
             for container in pod["containers"]:
                 stdin, stdout, stderr = ssh.exec_command(f"docker inspect {container['id']}")
